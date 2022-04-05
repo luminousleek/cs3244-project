5fbaf131c57e333d4d3692679741fb12e98709a7
wyrchris <wongyunruichris@gmail.com>
2022-04-05 19:11:32 +0800

    Add simple model
    Model follows Pytorch "Text classification with the torchtext library" tutorial, which uses an
    embedding bag layer and a linear layer.
    The dataset have not been split into training and test data, currently training and testing on
    the same dataset. The features used from the dataset to trained only uses the concatenation
    of the featrues: company_profile, description and requirements, as a single feature to train
    on.

--- dataset.py
+++ dataset.py
@@ -1,50 +1,39 @@
 import pandas
-import torch
 from torch.utils.data import Dataset
 from torchtext.vocab import build_vocab_from_iterator
-from torchtext.data.utils import get_tokenizer
 
-header_size = dict()
-vocab_list = dict()
-tokenizer = get_tokenizer('basic_english')
+features_to_use = ['company_profile', 'description', 'requirements']
 
 
-def get_header_size(df):
-    for header in df.columns:
-        size = 0
-        for item in df[header]:
-            size = max(size, len(str(item).split(" ")))
-        header_size[header] = size
+def build_vocab(df):
+    vocab_list = dict()
 
-
-def text_pipeline(text: str, header):
-    temp = vocab_list.get(header)(tokenizer(text))
-    temp += [0] * (header_size.get(header) - len(temp))
-    return temp
-
-
-def get_tensor(df):
     for header in df.columns:
         vocab = build_vocab_from_iterator([y for y in [str(x).split(" ") for x in df[header]]])
         vocab.set_default_index(0)
         vocab_list[header] = vocab
 
+    return vocab_list
+
+
+def combine_descriptions(df):
+    df['combined_description'] = ''
+    for feature in features_to_use:
+        df['combined_description'] += ' ' + df[feature]
+
 
 class JobPostingDataSet(Dataset):
     def __init__(self, filename):
         self.job_postings = pandas.read_csv(filename)
-        get_header_size(self.job_postings)
-        # tokenise(self.job_postings)
-        get_tensor(self.job_postings)
+        combine_descriptions(self.job_postings)
+        self.vocab_list = build_vocab(self.job_postings)
 
     def __len__(self):
         return len(self.job_postings)
 
     def __getitem__(self, idx):
         temp = self.job_postings.iloc[idx].to_dict()
-        label = temp["fraudulent"]
+        label = temp.get("fraudulent")
 
-        for header in self.job_postings.columns:
-            temp[header] = torch.tensor(text_pipeline(str(temp.get(header)), header))
-
-        return temp, label
+        text = ' '.join(list(map(lambda x: str(temp.get(x)), features_to_use)))
+        return text, label
---
+++ driver.py
@@ -0,0 +1,109 @@
+import time
+
+from torch import nn, torch
+from torch.utils.data import DataLoader, random_split
+from model import collate_batch, dataset, device
+
+
+class TextClassificationModel(nn.Module):
+    def __init__(self, _vocab_size, embed_dim, _num_class):
+        super(TextClassificationModel, self).__init__()
+        self.embedding = nn.EmbeddingBag(_vocab_size, embed_dim, sparse=True)
+        self.fc = nn.Linear(embed_dim, _num_class)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.5
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.fc.weight.data.uniform_(-initrange, initrange)
+        self.fc.bias.data.zero_()
+
+    def forward(self, text, offsets):
+        embedded = self.embedding(text, offsets)
+        return self.fc(embedded)
+
+
+training_data = dataset
+vocab = training_data.vocab_list.get('combined_description')
+
+num_class = len(set([label for (text, label) in training_data]))
+vocab_size = len(vocab)
+emsize = 128
+model = TextClassificationModel(vocab_size, emsize, num_class).to(device)
+
+
+def train(dataloader):
+    model.train()
+    total_acc, total_count = 0, 0
+    log_interval = 500
+    start_time = time.time()
+
+    for idx, (label, text, offsets) in enumerate(dataloader):
+        optimizer.zero_grad()
+        predicted_label = model(text, offsets)
+        loss = criterion(predicted_label, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+        optimizer.step()
+        total_acc += (predicted_label.argmax(1) == label).sum().item()
+        total_count += label.size(0)
+        if idx % log_interval == 0 and idx > 0:
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches '
+                  '| accuracy {:8.3f}'.format(epoch, idx, len(dataloader),
+                                              total_acc / total_count), elapsed)
+            total_acc, total_count = 0, 0
+            start_time = time.time()
+
+
+# evaluate
+def evaluate(dataloader):
+    model.eval()
+    total_acc, total_count = 0, 0
+
+    with torch.no_grad():
+        for idx, (label, text, offsets) in enumerate(dataloader):
+            predicted_label = model(text, offsets)
+            criterion(predicted_label, label)
+            total_acc += (predicted_label.argmax(1) == label).sum().item()
+            total_count += label.size(0)
+    return total_acc / total_count
+
+
+# training
+# Hyperparameters
+EPOCHS = 10  # epoch
+LR = 5  # learning rate
+BATCH_SIZE = 64  # batch size for training
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=LR)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.1)
+total_accu = None
+train_iter, test_iter = dataset, dataset
+train_dataset = train_iter
+test_dataset = test_iter
+num_train = int(len(train_dataset) * 0.95)
+split_train_, split_valid_ = random_split(train_dataset, [num_train, len(train_dataset) - num_train])
+
+train_dataloader = DataLoader(split_train_, batch_size=BATCH_SIZE,
+                              shuffle=True, collate_fn=collate_batch)
+valid_dataloader = DataLoader(split_valid_, batch_size=BATCH_SIZE,
+                              shuffle=True, collate_fn=collate_batch)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE,
+                             shuffle=True, collate_fn=collate_batch)
+
+for epoch in range(1, EPOCHS + 1):
+    epoch_start_time = time.time()
+    train(train_dataloader)
+    accu_val = evaluate(valid_dataloader)
+    if total_accu is not None and total_accu > accu_val:
+        scheduler.step()
+    else:
+        total_accu = accu_val
+    print('-' * 59)
+    print('| end of epoch {:3d} | time: {:5.2f}s | '
+          'valid accuracy {:8.3f} '.format(epoch,
+                                           time.time() - epoch_start_time,
+                                           accu_val))
+    print('-' * 59)
