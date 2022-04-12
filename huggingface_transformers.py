e6bb0a28e611e43bb36d6ce9d777c67622c39d9d
Isaac Lee <68138671+luminousleek@users.noreply.github.com>
2022-04-12 17:07:40 +0800

    add accuracy and f1 metrics

--- huggingface_transformers.py
+++ huggingface_transformers.py
@@ -1,10 +1,11 @@
 from transformers import AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification, TrainingArguments, Trainer
-import pandas as pd
-from datasets import load_dataset
+from datasets import load_dataset, load_metric
+import numpy as np
 # import logging
 
-dataset = load_dataset('csv', data_files={'train': 'train_data.csv', 'test': 'test_data.csv'})
-
+dataset = load_dataset('csv', data_files={'train': 'train_data_reduced.csv', 'test': 'test_data.csv'})
+acc_metric = load_metric('accuracy')
+f1_metric = load_metric('f1')
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
@@ -16,14 +17,22 @@ def preprocess_function(examples):
     return tokenized_batch
 
 
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    accuracy = acc_metric.compute(predictions=predictions, references=labels)
+    f1_score = f1_metric.compute(predictions=predictions, references=labels)
+    return {"accuracy": accuracy, "f1 score": f1_score}
+
+
 tokenized_df = dataset.map(preprocess_function, batched=True)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+model = AutoModelForSequenceClassification.from_pretrained("./models", num_labels=2)
 
 training_args = TrainingArguments(
     output_dir="./results",
-    learning_rate=2e-5,
+    learning_rate=1e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     num_train_epochs=1,
@@ -37,6 +46,10 @@ trainer = Trainer(
     eval_dataset=tokenized_df["test"],
     tokenizer=tokenizer,
     data_collator=data_collator,
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
+result = trainer.evaluate()
+print(result)
+# model.save_pretrained(save_directory="./models")
